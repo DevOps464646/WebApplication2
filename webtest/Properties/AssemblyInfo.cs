7e8af6dd322a6877df766d540fb850c6d11d01c0
Thomas Whiteley <tomazurefree@gmail.com>
2024-03-22 07:45:21 +0000

    Wrote unit test

---
+++ webtest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("webtest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("webtest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aad52192-6cb0-4502-adb4-625fefc7650f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ webtest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace webtest
+{
+    [TestClass]
+    public class DemoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i = 1;
+            bool result = false;
+            if (i == 1) result = true;
+            Assert.IsTrue(result, "Value should be equal to 1")
+        }
+    }
+}
